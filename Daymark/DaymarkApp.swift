03fe97b9867435d40ea25b3e52df9ec3a732cec7
Sreyas J S <133250113+Sreyas-J-S@users.noreply.github.com>
2026-08-20 10:12:58 +0530

    Initial Commit

---
+++ Daymark/DaymarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaymarkApp.swift
+//  Daymark
+//
+//  Created by sreyas on 20/08/26.
+//
+
+import SwiftUI
+
+@main
+struct DaymarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
